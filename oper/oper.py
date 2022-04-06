6b520af4f4eee2fd7b905fc2f51f0a795bea5730
Dante Basso <dcbasso@gmail.com>
2022-04-05 21:31:53 -0300

    assing, check and simple oper

---
+++ oper/oper.py
@@ -0,0 +1,9 @@
+valueOne = 1
+valueTwo = 2
+valueThree = valueOne + valueTwo
+print(valueThree)
+
+strHello = "hello"
+strWorld = "world"
+strHelloWorld = strHello + " " + strWorld
+print(strHelloWorld)
